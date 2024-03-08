326ce400b05d8e66ab8eba600ef1318b5a4f4e47
FiLKoNd <fil.yt.pass@gmail.com>
2024-03-08 14:57:16 +0300

    + методы для шрифта

--- src/main/java/com/filkond/megaclock/utils/FontUtils.java
+++ src/main/java/com/filkond/megaclock/utils/FontUtils.java
@@ -4,7 +4,25 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class FontUtils {
-    public static BufferedImage getBufferedImage(String text, Font font, int size) {
-        return null;
+    public static BufferedImage getImage(String text, Font font) {
+        var fm = getFontMetrics(text, font);
+        var img = new BufferedImage(fm.stringWidth(text), fm.getHeight(), BufferedImage.TYPE_INT_RGB);
+        var g2d = img.createGraphics();
+        g2d = img.createGraphics();
+        g2d.setFont(font);
+        fm = g2d.getFontMetrics();
+        g2d.setColor(Color.WHITE);
+        g2d.drawString(text, 0, fm.getAscent());
+        g2d.dispose();
+        return img;
+    }
+
+    private static FontMetrics getFontMetrics(String text, Font font) {
+        var bimg = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        var g2d = bimg.createGraphics();
+        g2d.setFont(font);
+        FontMetrics fm = g2d.getFontMetrics();
+        g2d.dispose();
+        return fm;
     }
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,30 +1,15 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.text.AttributedCharacterIterator;
-import java.text.AttributedString;
+
+import static com.filkond.megaclock.utils.FontUtils.getImage;
 
 public class Main {
     public static void main(String[] args) {
         var time = "12:05:15";
-        var bimg = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-        var g2d = bimg.createGraphics();
-        var font = new Font("Times New Roman", Font.PLAIN, 20);
-        g2d.setFont(font);
-        FontMetrics fm = g2d.getFontMetrics();
-        int width = fm.stringWidth(time);
-        int height = fm.getHeight();
-        g2d.dispose();
-
-        bimg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        g2d = bimg.createGraphics();
-        g2d.setFont(font);
-        fm = g2d.getFontMetrics();
-        g2d.setColor(Color.WHITE);
-        g2d.drawString(time, 0, fm.getAscent());
-        g2d.dispose();
+        var font = new Font("Times New Roman", Font.PLAIN, 45);
+        var bimg = getImage(time, font);
         try {
             ImageIO.write(bimg, "png", new File("test.png"));
         } catch (IOException ex) {
